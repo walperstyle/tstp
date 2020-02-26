54a562fda69dfbef3d3d99ae03b329acf62ba6b0
walperstyle <61523732+walperstyle@users.noreply.github.com>
2020-02-26 15:54:02 -0700

    Update ch3_challenge6.py

--- part_I/introduction_to_programming/challenges/ch3_challenge6.py
+++ part_I/introduction_to_programming/challenges/ch3_challenge6.py
@@ -1,7 +1,13 @@
-age = 64
-retirement = age - 65
+age = 62
+retirement =  65 - age
 
-if retirement < 10:
+if retirement < 10 and retirement >= 0:
     print("You get to retire soon.")
+elif retirement <= 0:
+    print("You should already be retired")
 else:
     print("You have a long time until you can retire!")
+    
+#the old code for challenge 6, page 45 from the book was wrong
+#the old code would produce "you get to retire soon" regardless
+#of what the age variable was. This code I have written is better.
